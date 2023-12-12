0adb47eb43254aa76892feab27bfbcaba00891cd
Kavikumar613 <146411658+Kavikumar613@users.noreply.github.com>
2023-12-12 14:55:38 +0530

    add all images file

---
+++ images/Coming Soon/Search.js
@@ -0,0 +1,429 @@
+const Allmovies = [
+    {
+        name: "apurva",
+        poster: "/images/Latest movies/Apurva.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Latest movies/JFK.jpg "
+    },
+    {
+        name: "Aarya",
+        poster: "/images/Latest movies/Aarya.png"
+    },
+    {
+        name: "bigboss7",
+        poster: "/images/Latest movies/BB7.jpg"
+    },
+    {
+        name: "chittha",
+        poster: "/images/Latest movies/chitha.jpg"
+    },
+    {
+        name: "vaalaty",
+        poster: "/images/Latest movies/Valatty.jpeg"
+    },
+    {
+        name: "label",
+        poster: "/images/Latest movies/Label.png"
+    },
+    {
+        name: "spiderman",
+        poster: "/images/Latest movies/Spider-Man-Into-The-Spider-Verse.jpg"
+    },
+    {
+        name: "vaccine",
+        poster: "/images/Latest movies/Vaccine-war.jpg"
+    },
+    {
+        name: "skanda",
+        poster: "/images/Latest movies/Skanda.webp"
+    },
+    {
+        name: "kannusquad",
+        poster: "/images/Latest movies/kannur-squad.jpg"
+    },
+    {
+        name: "homeintheworld",
+        poster: "/images/Latest movies/Home in the world.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Latest movies/National geo.jpg"
+    },
+    {
+        name: "cuttpulli",
+        poster: "/images/Free movies/CP.jpg"
+    },
+    {
+        name: "freddy",
+        poster: "/images/Free movies/F.webp"
+
+    },
+    {
+        name: "humdo",
+        poster: "/images/Free movies/H-D.jpg"
+    },
+    {
+        name: "vikram",
+        poster: "/images/Free movies/Vikram.webp"
+    },
+    {
+        name: "bhessman",
+        poster: "/images/Free movies/B.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Free movies/T P I.jpeg"
+    },
+    {
+        name: "cuddavar",
+        poster: "/images/Free movies/C.webp"
+    },
+    {
+        name: "laxmi",
+        poster: "/images/Free movies/L.jpg"
+    },
+    {
+        name: "hungama2",
+        poster: "/images/Free movies/HG-2.jpg"
+    },
+    {
+        name: "liger",
+        poster: "/images/Free movies/Liger.jpeg"
+    },
+    {
+        name: "raangi",
+        poster: "/images/Free movies/rangi.jpeg"
+    },
+    {
+        name: "sanak",
+        poster: "/images/Free movies/Sanak.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Free movies/VR.jpg"
+    },  
+    {
+        name: "kanaakaanumkaalangal",
+        rank: "4",
+        poster: "/images/top 10/KK.webp"
+    },
+    {
+        name: "label",
+        poster: "/images/Hostar-special/HP-1.png"
+    },
+    {
+        name: "koffeewithkaran",
+        poster: "/images/Hostar-special/HP-3.png"
+    },
+    {
+        name: "masterpeace",
+        poster: "/images/Hostar-special/HP-4.jpg"
+    },
+    {
+        name: "manssion24",
+        poster: "/images/Hostar-special/HP-5.jpg"
+    },
+    {
+        name: "sultanofdelhi",
+        poster: "/images/Hostar-special/HP-6.png"
+    },
+    {
+        name: "athidhi",
+        poster: "/images/Hostar-special/HP-7.jpeg"
+    },
+    {
+        name: "my3",
+        poster: "/images/Hostar-special/HP-8.png"
+    },
+    {
+        name: "kaala",
+        poster: "/images/Hostar-special/HP-9.png"
+    },
+    {
+        name: "freelancer",
+        poster: "/images/Hostar-special/HP-10.jpg"
+    },
+    {
+        name: "aakhrisach",
+        poster: "/images/Hostar-special/HP-11.jpg"
+    },
+    {
+        name: "mathagam",
+        poster: "/images/Hostar-special/HP-12.jpeg"
+    },
+    {
+        name: "commandu",
+        poster: "/images/Hostar-special/HP-13.jpeg"
+    },
+    {
+        name: "trial",
+        poster: "/images/Hostar-special/HP-14.jpeg"
+    },
+    {
+        name: "thenightmanager",
+        poster: "/images/Hostar-special/HP-15.webp"
+    },
+    {
+        name: "keralacrimefile",
+        poster: "/images/Hostar-special/HP-16.jpg"
+    },
+    {
+        name: "shaithan",
+        poster: "/images/Hostar-special/HP-17.jpg"
+    },
+    {
+        name: "dayaa",
+        poster: "/images/Hostar-special/HP-18.webp"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-0.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-1.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-2.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-3.png"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-4.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-5.png"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-6.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-7.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-8.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Coming Soon/CS-9.webp"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero1.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero2.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero3.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero4.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero5.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero6.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero7.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero8.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero9.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero10.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero11.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero12.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero13.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero14.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero15.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero16.webp "
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero17.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero18.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Marvel/Hero19.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img1.webp"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img2.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img3.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img4.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img5.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img6.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img7.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img8.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img9.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img10.jpg "
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img11.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img12.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img13.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img14.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img15.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img16.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Disney/img17.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img1.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/Frozen-II-Magic.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img2.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img3.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img4.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img5.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img6.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img7.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img8.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img9.jpg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img10.webp"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img11.png"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img12.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img13.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img14.jpeg"
+    },
+    {
+        name: "",
+        poster: "/images/Kids/img15.jpg"
+    }
+
+];
